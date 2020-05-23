ad7907131f484b95191e41f1b0250807bab5a92a
ahad <mdahad600@gmail.com>
2020-05-23 11:01:44 +0600

    Updates

---
+++ precache-manifest.bd25b0790670ac16a7ddc4ed9a4c0126.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adb3fb89a8b0f5fe33533e69a7b9aa5b",
+    "url": "/project_1-react-/index.html"
+  },
+  {
+    "revision": "9d60f9fde20dd3dce0f6",
+    "url": "/project_1-react-/static/css/main.51303337.chunk.css"
+  },
+  {
+    "revision": "4a24d1321a2df850daa5",
+    "url": "/project_1-react-/static/js/2.7c669956.chunk.js"
+  },
+  {
+    "revision": "497d7e07705261348096eb742962e0f0",
+    "url": "/project_1-react-/static/js/2.7c669956.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9d60f9fde20dd3dce0f6",
+    "url": "/project_1-react-/static/js/main.71f89c08.chunk.js"
+  },
+  {
+    "revision": "a526d1f6bcac3adc05c8",
+    "url": "/project_1-react-/static/js/runtime-main.653395ad.js"
+  },
+  {
+    "revision": "575e4317521b381ac94c0c8207c81979",
+    "url": "/project_1-react-/static/media/Raleway-Bold.575e4317.ttf"
+  },
+  {
+    "revision": "9f5eec4e61754abf89124df236b87358",
+    "url": "/project_1-react-/static/media/Raleway-Thin.9f5eec4e.ttf"
+  },
+  {
+    "revision": "039e7c28a4230e9aba914efde06324cb",
+    "url": "/project_1-react-/static/media/bg-3.039e7c28.jpg"
+  },
+  {
+    "revision": "eb77e1ec20848872bd5672b974ff2b8f",
+    "url": "/project_1-react-/static/media/bg-4.eb77e1ec.jpg"
+  }
+]);
